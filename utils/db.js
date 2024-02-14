496a1b6a9fd6e80c592e9e1a2b498b4bf81c8814
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-02-14 20:25:21 +0200

    Mongodb-client

---
+++ utils/db.js
@@ -0,0 +1,44 @@
+// Class DBClient
+import MongoClient from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const {
+      DB_HOST = 'localhost',
+      DB_PORT = 27017,
+      DB_DATABASE = 'files_manager',
+    } = process.env;
+
+    this.dbhost = DB_HOST;
+    this.dbport = DB_PORT;
+    this.dbdatabase = DB_DATABASE;
+
+    this.client = new MongoClient(`mongodb://$(this.dbhost):$(this.dbport)`);
+    this.client.connect();
+    this.db = this.client.db(this.dbdatabase);
+  }
+
+  isAlive() {
+    try {
+      this.client.db().admin().ping();
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async nbUsers() {
+    const usersCollection = this.db.collection('users');
+    const count = await usersCollection.countDocument();
+    return count;
+  }
+
+  async nbFiles() {
+    const filesCollection = this.db.collection('files');
+    const count = await filesCollection.countDocument();
+    return count;
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
