87396ba22b04d62fc4f7830bf66898640d8a5df2
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-05-18 00:10:15 +0200

    Commit

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // server.js
 const express = require('express');
-const router = require('./routes');
+const routes = require('./routes/index');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use('/', router);
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
